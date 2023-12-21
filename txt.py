0f8ce73d68718f8e06dd75784a15f853b88833f0
tanvikulkarni1211 <tanvi.logicwind@gmail.com>
2023-12-21 15:01:05 +0530

    initaial commit

---
+++ txt.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
